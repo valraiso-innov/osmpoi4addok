a9cd8923278fb013d9fdfeff9fd28d2c2cb3735c
Yoann Couble <yoann.couble@valraiso.fr>
2023-05-12 12:03:11 +0200

    customized for tourism

--- 30-extract-poi.sh
+++ 30-extract-poi.sh
@@ -1,8 +1,8 @@
 PBF_NAME=france_metro_dom_com_nc.osm.pbf
 PBF_URL=http://download.openstreetmap.fr/extracts/merge/$PBF_NAME
-wget -N $PBF_URL
+#wget -N $PBF_URL
 
-FILTER="`tail -n +2 def.csv | cut -d ',' -f 1,2,3 | sort | sed -e 's/"//g;s/\([-_a-z0-9]\+\),\([-_a-z0-9]\+\),\([-_a-z0-9]*\)/( \1=\2 and \3= ) or/' | tr '\n' ' ' | sed -e 's/ and =//g' | sed -e 's/ or \$//'`"
+FILTER="`tail -n +2 def.csv | cut -d ',' -f 1,2 | sort | sed -E -e 's/"//g;s/([-_a-z0-9]+),([-_a-z0-9]+)/\1=\2 or/' | tr '\n' ' ' | sed -E -e 's/ and =//g' | sed -E -e 's/ or %$//'`"
 osmconvert $PBF_NAME -o=latest.o5m
 osmfilter latest.o5m --keep="$FILTER" -o=poi.o5m
 osmconvert poi.o5m --all-to-nodes --add-bbox-tags --max-objects=1000000000 -o=nodes.o5m
